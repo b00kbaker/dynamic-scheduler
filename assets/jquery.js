e084a75b670b75723a10875dcab2c213b0f2f57b
b00kbaker <caimmurphy@gmail.com>
2021-01-26 20:03:40 -0700

    Psuedo code for js plus linked dayjs for time library

--- assets/jquery.js
+++ assets/jquery.js
@@ -1,1 +1,31 @@
-jquery
+// jquery
+
+
+
+console.log(dayjs(), "Dayjs")
+
+// Figure out time/date plug in
+
+// Global var
+  //var whatPlans
+  //   
+
+// Functions
+
+// function saveHour()
+
+
+// How will it work?
+  //As each hour passes the color gets faded out and no more interation is allowed (if statmenent? if time = past no action on "done" button or editing text box)
+
+ //Time is only 9am-5pm   (diff)
+
+ //Future time in 1 color
+ //Past is 2 color
+ //Present is 3 color
+ 
+ //Reset color on every hour (event listener on time?)  
+
+ //When "save" is clicked add text plans + assigned time to localStorage  
+
+ //Recognized new day (AKA midnight) and all text empties
