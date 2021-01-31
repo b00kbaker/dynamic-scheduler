fac10458424b85397c39ddf1504e86b692a1ad05
b00kbaker <caimmurphy@gmail.com>
2021-01-31 15:30:29 -0700

    Feeling lost

--- assets/jquery.js
+++ assets/jquery.js
@@ -1,49 +1,92 @@
+// Global variables
+var whatPlans = [];
+var currentDate = dayjs().date();
+var currentTime = dayjs();
+var pastHour = [];
+var presentHour = dayjs().format("ha");
+var futureHour = [];
+var workHours = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
 
 
 
-  var whatPlans=[]
-  var currentDate=dayjs().date()
-  var currentTime=dayjs()
-  var pastHour=[]
-  var presentHour=dayjs().hour()
-  var futureHour=[] 
-  var workHours= ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]   
+$("#currentDay").text(currentTime.format("DD/MM/YYYY h:mm a"));
+
+for (i = 0; i < workHours.length; i++) {
+  $(`<div class="col-2 time-block"></div>`).text(workHours[i]).appendTo(".row");
+  $(`<textarea class="col-8 note-input" id="input-${i}"></textarea>`)
+    .attr("placeholder", "Enter note here")
+    .appendTo(".row");
+  $(`<button class="col-2 btn btn-secondary saveBtn" id="hour${i}"></button>`)
+    .text("Save")
+    .appendTo(".row");
+};
 
 
-$("#currentDay").text(currentTime.format('DD/MM/YYYY h:mm a'));
 
-// console.log()
+
+
+  // } else if (Number(dateHour) < dayjs().hour()) {
+  //   $(this).find('textarea').addClass('past').attr('disabled', 'disabled');
+  //   $(this).find('.save-button').addClass('disabled').attr('disabled', true);
+  // } else {
+  //   $(this).find('textarea').addClass('future');
+  // }
+  
+
+  
+
+
+
+// function checkTime (){
+//   if(currentTime.format("ha") === presentHour){
+//     .addClass('present')} 
+//   }
+// ;
+
+
+
+//     else (pastHour < currentTime.format("ha"))
+//       $(this).find('textarea').addClass('past')}
+//       else(futureHour > currentTime.format("ha"))
+//     }
+   
+// }
+//   };
+// };   
+
+
+
+
+
+console.log (currentTime.format("ha"))
+
 
 // Functions
 
 //  function checkTime()
-   // Run every time the page loads/refreshes (contains if/else staments about past, present, future)
+// Run every time the page loads/refreshes (contains if/else staments about past, present, future)
 
- // if (workHours > workHours.length - 1) {
+// if (workHours > workHours.length - 1) {
 //     newDay();
 //     return;
-
-
 
 // function saveHour()
 
 // function newDay()
-   //reset all time blocks to future (color)
-   //empty all txt blocks
-   //empty local storage?    
-
+//reset all time blocks to future (color)
+//empty all txt blocks
+//empty local storage?
 
 // How will it work?
-  //As each hour passes the color gets faded out and no more interation is allowed (if statmenent? if time = past no action on "done" button or editing text box)
+//As each hour passes the color gets faded out and no more interation is allowed (if statmenent? if time = past no action on "done" button or editing text box)
 
- //Time is only 9am-5pm   (diff)
-
- //Future time in 1 color --All colors are already plugged into CSS
- //Past is 2 color
- //Present is 3 color
- 
- //Reset color on every hour (event listener on time?)  
-
- //When "save" is clicked add text plans + assigned time to localStorage  
-
- //Recognized new day (AKA midnight) and all text empties
