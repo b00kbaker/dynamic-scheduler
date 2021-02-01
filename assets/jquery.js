d574fc820eae19b4151531204ae33829ca168448
b00kbaker <caimmurphy@gmail.com>
2021-01-31 22:29:09 -0700

    Unresolved on localStorage

--- assets/jquery.js
+++ assets/jquery.js
@@ -1,4 +1,5 @@
 // Global variables
+var whatPlans;
 var currentDate = dayjs().date();
 var currentTime = dayjs();
 var presentHour = dayjs().format("ha");
@@ -7,34 +8,30 @@ var workHours = [
   "10am",
   "11am",
   "12pm",
-  "1pm", 
+  "1pm",
   "2pm",
   "3pm",
   "4pm",
   "5pm",
 ];
 
-if (localStorage.getItem("localHourlyTasks")) {
-  whatPlans= JSON.parse(localStorage.getItem("localHourlyTasks"));
-} else {
-  whatPlans = [];
-};
-
+// if (localStorage.getItem("localHourlyTasks")) {
+//   whatPlans= JSON.parse(localStorage.getItem("localHourlyTasks"));
+// } else {
+//   whatPlans = [];
+// };
 
 $("#currentDay").text(currentTime.format("DD/MM/YYYY h:mm a"));
 
 for (i = 0; i < workHours.length; i++) {
-  var row= $(`<div class="row"</div>`).appendTo(".container");
-  let timeClass=""
-  if(workHours[i] === presentHour){
-    timeClass="col-8 note-input present"
-  }
-  else if (workHours[i] < presentHour) {
-    timeClass="col-8 note-input future"
-  } 
-  else (workHours[i] > presentHour)
-    timeClass="col-8 note-input past"
-
+  var row = $(`<div class="row"</div>`).appendTo(".container");
+  let timeClass = "";
+  if (workHours[i] === presentHour) {
+    timeClass = "col-8 note-input present";
+  } else if (workHours[i] < presentHour) {
+    timeClass = "col-8 note-input future";
+  } else workHours[i] > presentHour;
+  timeClass = "col-8 note-input past";
 
   $(`<div class="col-2 time-block"></div>`).text(workHours[i]).appendTo(row);
   $(`<textarea class="${timeClass}" id="hour${i}"></textarea>`)
@@ -44,24 +41,20 @@ for (i = 0; i < workHours.length; i++) {
     .text("Save")
     .appendTo(row);
 
-    console.log(presentHour)
-};
+  console.log(presentHour);
+}
 
-document.querySelector(".saveBtn").addEventListener("click", function(event) {
+document.querySelector(".saveBtn").addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(event.target.id)
-  console.log($(`textarea#${event.target.id}`).val())
-  localStorage.setItem(event.target.id,$(`textarea#${event.target.id}`).val())
-  localStorage.setItem(event.target.id,$(`textarea#${event.target.id}`).val())
-
+  console.log(event.target.id);
+  console.log(event.target.id[4]);
+  console.log(event.target.id);
+  console.log($(`textarea#${event.target.id}`).val());
+  localStorage.setItem(event.target.id, $(`textarea#${event.target.id}`).val());
 });
-
 
 // Functions
 
-
-
-// if (workHours > workHours.length - 1) {
 //     newDay();
 //     return;
 
@@ -71,7 +64,3 @@ document.querySelector(".saveBtn").addEventListener("click", function(event) {
 //reset all time blocks to future (color)
 //empty all txt blocks
 //empty local storage?
-
-// How will it work?
-//As each hour passes the color gets faded out and no more interation is allowed (if statmenent? if time = past no action on "done" button or editing text box)
-
