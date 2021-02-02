6cf75e942c08741c5b8e0c4b1231cb80e0784675
b00kbaker <caimmurphy@gmail.com>
2021-02-01 18:25:27 -0700

    Finally am defining key/value for localStorage and successful with setItem/getItem

--- assets/jquery.js
+++ assets/jquery.js
@@ -1,4 +1,5 @@
 var whatPlans;
+var text;
 var currentDate = dayjs().date();
 var currentTime = dayjs();
 var presentHour = dayjs().format("ha");
@@ -14,7 +15,6 @@ var workHours = [
   "5pm",
 ];
 
-
 // if (localStorage.getItem("localHourlyTasks")) {
 //   whatPlans= JSON.parse(localStorage.getItem("localHourlyTasks"));
 // } else {
@@ -23,53 +23,54 @@ var workHours = [
 
 $("#currentDay").text(currentTime.format("DD/MM/YYYY h:mm a"));
 
-for (i = 0; i < workHours.length; i++) {
+for (let i = 0; i < workHours.length; i++) {
   var row = $(`<div class="row"</div>`).appendTo(".container");
   let timeClass = "";
-  if (workHours[i] < presentHour) 
-    timeClass = "col-8 note-input past";
-  else if (workHours[i] > presentHour) 
-    timeClass = "col-8 note-input future";
-  else if (workHours[i] = presentHour) 
-    timeClass = "col-8 note-input present";
+  if (workHours[i] < presentHour) timeClass = "col-8 note-input past";
+  else if (workHours[i] > presentHour) timeClass = "col-8 note-input future";
+  else if (workHours[i] == presentHour) timeClass = "col-8 note-input present";
 
   $(`<div class="col-2 time-block"></div>`).text(workHours[i]).appendTo(row);
-  $(`<textarea class="${timeClass}" id="hour${i}"></textarea>`)
+ var txt= $(`<textarea class="${timeClass}" id="hour${i}"></textarea>`)
     .attr("placeholder", "Enter note here")
+    
     .appendTo(row);
   $(`<button class="col-2 btn btn-secondary saveBtn" id="hour${i}"></button>`)
     .text("Save")
     .appendTo(row);
 
+  var saveText= localStorage.getItem(workHours[i]);
+txt.val(saveText)
   console.log(presentHour);
   console.log(workHours[i]);
-};
-
-document.querySelector(".saveBtn").addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log(event.target.id);
-  // console.log($`textarea#${"#hour${i}"}`);
-  // console.log($(`textarea#${event.target.id}`).val());
-  localStorage.setItem(event.target.id, $(`textarea#${event.target.id}`).val());
-});
-
-
-
-
-
-// function newDay()
-if (currentTime = "12am"){
 }
 
-//reset all time blocks to future (color)
-workHours.timeClass = "col-8 note-input future";
+$(".saveBtn").on("click", function (event) {
+  event.preventDefault();
+  console.log($(this).siblings("div").text());
+  console.log($(this).siblings("textarea").val());
+  text = $(this).siblings("textarea").val();
+  var timeSchedule = $(this).siblings("div").text();
+  // console.log($`textarea#${"#hour${i}"}`);
+  // console.log($(`textarea#${event.target.id}`).val());
+  localStorage.setItem(timeSchedule, text);
+  
+});
 
-//empty all txt blocks
-all <textarea> to .placeholder
+// var saveText= localStorage.getItem(text);
+// txt.val(saveText)
+//Do I have to have an array to push info to? Still issue with defining/selecting all time blocks (aka key). Each time block has an id of "hour0"-"hour8", should I just make an if statement for each one?  or  use var hourBlocks=["hour0", "hour1"]?
+
+// function newDay()
+///////////////////////}
+
+//reset all time blocks to future (color)
+// workHours.timeClass = "col-8 note-input future";
+
+//empty all txt blocks////////////////////
+// all <textarea> to .placeholder
 
 //empty local storage?
-localStorage.clear();
-
-
+// localStorage.clear();
 
 // newDay(); Check this function every time the page is loaded
